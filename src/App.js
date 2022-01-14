a990a5440899a90e7e6126ede9f8318f7acfa24c
Dikshant Kunwar <dikshantkunwar@gmail.com>
2022-01-14 01:21:32 -0500

    separated weather api function as a custom hook

--- src/App.js
+++ src/App.js
@@ -1,12 +1,51 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import Counter from './component/Counter';
 
+import axios from 'axios'
 import useCounter from './hooks/useCounter';
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?q='
 
 function App() {
   const [count, increment, decrement, reset] = useCounter(0);
+  const [city, setCity]  = useState('london');
+  const [weather, setWeather] = useState(null);
+  const [location, setLocation] = useState(null);
+  
+
+  useEffect(() => {
+    axios
+      .get(url + city + '&appid=97c3e96f6f3c34c11624097dc5b80b8d')
+      .then(res => {
+        const data = res.data;
+        setWeather(data.weather);
+        setLocation(data.name);
+      });
+  },)
   return (
     <div className="App">
+      {/*
+        --- weather display component will have 
+          --- weather icon 
+            --- degrees 
+      <WeatherDisplay 
+        {...weather}
+      />
+
+      <LocationDisplay 
+        {..location}
+      <
+       */}
+       <div>
+         <p>
+           {weather}
+         </p>
+         <p>
+           {location}
+         </p>
+       </div>
+
       <Counter
         count = {count}
         increment = {increment}
@@ -18,6 +57,7 @@ function App() {
         increment = {increment}
         decrement = {decrement}
         reset = {reset}
+        onClick={setCity}
       />
     </div>
   );
